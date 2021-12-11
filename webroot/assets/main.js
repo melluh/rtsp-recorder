4f2cc4325acbe5afd82c7ae6c584fc52468a084e
MisterMelDev <info@melluh.com>
2021-12-11 20:02:28 +0100

    Add playback speed dropdown

--- webroot/assets/main.js
+++ webroot/assets/main.js
@@ -3,6 +3,7 @@ const dateSelect = document.getElementById("date-select");
 const recordingsContainer = document.getElementById("recordings");
 const videoView = document.getElementById("video-view");
 const nowViewingElement = document.getElementById("now-viewing");
+const speedSelect = document.getElementById("video-speed");
 
 // Set default and max value to today
 dateSelect.valueAsDate = new Date();
@@ -13,6 +14,8 @@ cameraSelect.addEventListener("change", () => loadRecordings());
 dateSelect.addEventListener("change", () => loadRecordings());
 
 var cameras = [];
+var recordings = [];
+var currentRecording = undefined;
 
 function loadStatus() {
     fetch("/api/status").then(r => r.json()).then(json => {
@@ -28,7 +31,8 @@ function updateCamerasDropdown() {
     var optHeader = document.createElement("option");
     cameraSelect.appendChild(optHeader);
     optHeader.innerHTML = "Select a camera";
-    optHeader.disabled = true;
+    optHeader.disabled = true
+    optHeader.value = "none";
 
     cameras.forEach(camera => {
         var opt = document.createElement("option");
@@ -40,7 +44,12 @@ function updateCamerasDropdown() {
 function loadRecordings() {
     recordingsContainer.innerHTML = "";
 
+    if(cameraSelect.value === "none") {
+        return;
+    }
+
     fetch("/api/recordings?camera=" + cameraSelect.value + "&date=" + formatDate(dateSelect.valueAsDate)).then(r => r.json()).then(json => {
+        recordings = json;
         json.forEach(recording => {
             var recordingElement = document.createElement("div");
             recordingElement.classList.add("recording");
@@ -56,14 +65,8 @@ function loadRecordings() {
             playIcon.classList.add("recording-view-icon")
             recordingElement.appendChild(playIcon);
 
-            playIcon.addEventListener("click", e => {
-                videoView.src = "/recordings/" + recording.filePath;
-                videoView.play();
-                nowViewingElement.innerHTML = "Now viewing: <a href=\"/recordings/" + recording.filePath + "?download=1\">" + recording.filePath + "</a>";
-
-                Array.from(document.querySelectorAll(".recording-view-icon-active")).forEach((el) => el.classList.remove("recording-view-icon-active"));
-                playIcon.classList.add("recording-view-icon-active");
-            });
+            playIcon.addEventListener("click", () => playRecording(recording));
+            recording.playIcon = playIcon;
         });
     });
 }
@@ -77,3 +80,27 @@ function pad(num, size) {
     while(num.length < size) num = "0" + num;
     return num;
 }
+
+speedSelect.addEventListener("change", () => updateSpeed());
+
+function playRecording(recording) {
+    currentRecording = recording;
+    videoView.src = "/recordings/" + recording.filePath;
+    videoView.play();
+    updateSpeed();
+    nowViewingElement.innerHTML = "Now viewing: <a href=\"/recordings/" + recording.filePath + "?download=1\" title=\"Click to download file\">" + recording.filePath + "</a>";
+
+    Array.from(document.querySelectorAll(".recording-view-icon-active")).forEach((el) => el.classList.remove("recording-view-icon-active"));
+    recording.playIcon.classList.add("recording-view-icon-active");
+}
+
+function updateSpeed() {
+    videoView.playbackRate = speedSelect.value;
+}
+
+videoView.addEventListener("ended", () => {
+    var index = recordings.indexOf(currentRecording);
+    if(index >= 0 && index < recordings.length - 1) {
+        playRecording(recordings[index + 1]);
+    }
+});
